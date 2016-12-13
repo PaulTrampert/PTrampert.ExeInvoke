90100282d61617ef6409f161bd9fc423c02fde06
Paul Trampert <paul.trampert@gmail.com>
2016-12-13 17:54:10 -0500

    Commit files that VS decided to exclude for some reason.

--- PTrampert.ExeInvoke.Test/TestBat.cmd
+++ PTrampert.ExeInvoke.Test/TestBat.cmd
@@ -4,6 +4,10 @@ echo This output goes to std out
 
 echo This output goes to std err>&2
 
+if defined testVar echo %testVar%
+
+echo Working Directory: %cd%
+
 set arg1 = %1
 
 if [%arg1%]==[] set arg1 = 0
---
+++ PTrampert.ExeInvoke/IExeInvoker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PTrampert.ExeInvoke
+{
+    public interface IExeInvoker
+    {
+        IDictionary<string, string> EnvironmentVariables { get; set; }
+        Func<StreamReader, Task> StandardErrReader { get; set; }
+        Func<StreamReader, Task> StandardOutReader { get; set; }
+        string WorkingDirectory { get; set; }
+
+        Task Invoke(string exe, params string[] args);
+    }
+}
